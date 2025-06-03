4b797956b7dfa3f43bdab384735ebb013ff97e5c
Daz <djones@yozu.co.uk>
2025-06-03 17:23:31 +0100

    corrected export

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import "./lib/string.js";
 import "./lib/array.js";
 import "./lib/number.js";
 import "./lib/object.js";
-export { $ } from "./lib/utilities.js";
+export { $ } from "./lib/utility.js";
